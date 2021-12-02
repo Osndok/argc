732d14de4149d2f9c89a0d6a0c9690cd2a74ad2a
Robert Hailey <git@osndok.com>
2021-12-01 21:56:52 -0600

    Repurpose related project to simply print the number of arguments

--- src/main.zig
+++ src/main.zig
@@ -1,25 +1,8 @@
 const std = @import("std");
-const log = std.log;
 const out = std.io.getStdOut().writer();
 
 pub fn main() anyerror!void
 {
-    const allocator = std.heap.GeneralPurposeAllocator(.{}){};
-    //???: defer std.debug.assert(!allocator.deinit());
-
-    //var i = std.process.args();
-    
-    //while (i.hasNext())
-    {
-        //var n = i.next(allocator);
-        
-    }
-    
-    for (std.os.argv) |a, i|
-    {
-        if (i > 0)
-        {
-            log.info("arg: {s}", .{a});
-        }
-    }
+    const args = std.os.argv;
+    try out.print("{d}\n", .{args.len-1});
 }
